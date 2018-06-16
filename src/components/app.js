f4ee4fc57041282cb993a99e1aeaa24262e9d3ab
Shashi <skumar335@sapient.com>
2018-06-16 12:13:14 +0530

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../styles/app.scss';
+
+export default (
+    <div>
+        <h1>JSX</h1>
+        <span>My first JSX span!</span>
+    </div>
+);
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+import './styles/index.scss';
+
+const container = document.getElementById('container');
+
+ReactDOM.render(App, container);
